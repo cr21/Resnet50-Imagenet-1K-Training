60de28103b83fdc0220ba9d7ed74f1dbf018abc2
Chirag Tagadiya <chiragtagadiya@Chirags-MacBook-Pro.local>
2025-10-19 01:22:39 -0500

    lr_finder added

--- lr_finder.py
+++ lr_finder.py
@@ -2,17 +2,17 @@ from torch_lr_finder import LRFinder
 import torch
 import torchvision
 import torchvision.transforms as transforms
-from model import ResNet50
+from model import ResNet50Wrapper
 import torch.nn as nn
 import torch.optim as optim
-from train import Params
+from utils import Config
 import fire
 import matplotlib.pyplot as plt
 from datetime import datetime
 import os
 
 def find_lr(start_lr=1e-7, end_lr=10, num_iter=100, output_dir='lr_finder_plots'):
-    params = Params()
+    config = Config()
     print(f"Find LR with params: Start_lr: {start_lr}, End_lr: {end_lr}, Num_iter: {num_iter}")
     device = (
         "cuda"
@@ -24,12 +24,12 @@ def find_lr(start_lr=1e-7, end_lr=10, num_iter=100, output_dir='lr_finder_plots'
 
     print(f"Using {device} device")
     
-    training_folder_name = '/mnt/imagenet/ILSVRC/Data/CLS-LOC/train'
+    training_folder_name = config.train_folder_name
     train_transformation = transforms.Compose([
         transforms.ToTensor(),
-        transforms.RandomResizedCrop(224, interpolation=transforms.InterpolationMode.BILINEAR, antialias=True),
+        transforms.RandomResizedCrop(config.IMG_W, interpolation=transforms.InterpolationMode.BILINEAR, antialias=True),
         transforms.RandomHorizontalFlip(0.5),
-        transforms.Normalize(mean=[0.485, 0.485, 0.406], std=[0.229, 0.224, 0.225])
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
 
     train_dataset = torchvision.datasets.ImageFolder(
@@ -39,15 +39,20 @@ def find_lr(start_lr=1e-7, end_lr=10, num_iter=100, output_dir='lr_finder_plots'
     
     train_loader = torch.utils.data.DataLoader(
         train_dataset,
-        batch_size=params.batch_size,
+        batch_size=config.batch_size,
         shuffle=True,
-        num_workers=params.workers,
+        num_workers=config.workers,
         pin_memory=True
     )
 
-    model = ResNet50(num_classes=len(train_dataset.classes)).to(device)
+    model = ResNet50Wrapper(num_classes=len(train_dataset.classes)).to(device)
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(model.parameters(), lr=start_lr, momentum=params.momentum, weight_decay=params.weight_decay)
+    optimizer = optim.SGD(
+        model.parameters(), 
+        lr=start_lr, 
+        momentum=config.momentum, 
+        weight_decay=config.weight_decay
+    )
 
     lr_finder = LRFinder(model, optimizer, criterion, device=device)
     lr_finder.range_test(train_loader, start_lr=start_lr, end_lr=end_lr, num_iter=num_iter, step_mode="exp")
